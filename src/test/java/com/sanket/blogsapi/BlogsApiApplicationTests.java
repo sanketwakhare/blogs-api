81cb1eb046c80bb1be4eee0ac863602635086ce7
sanketwakhare <sanketwakhare@gmail.com>
2023-05-06 13:42:40 +0530

    add test to create user

--- src/test/java/com/sanket/blogsapi/BlogsApiApplicationTests.java
+++ src/test/java/com/sanket/blogsapi/BlogsApiApplicationTests.java
@@ -1,13 +1,26 @@
 package com.sanket.blogsapi;
 
+import com.sanket.blogsapi.users.UserEntity;
+import com.sanket.blogsapi.users.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BlogsApiApplicationTests {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void testCreateUser() {
+        UserEntity userEntity = new UserEntity("Sanket1234", "sanket1234@example.com", "password", "bio", null, null);
+        UserEntity savedUser = userRepository.save(userEntity);
+        System.out.println(savedUser);
     }
 
 }
