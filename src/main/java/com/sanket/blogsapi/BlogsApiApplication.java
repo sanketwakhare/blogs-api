22ae923ced452656ca05852936943188add582ce
sanketwakhare <sanketwakhare@gmail.com>
2023-05-14 20:15:19 +0530

    add model mapper bean configuration

--- src/main/java/com/sanket/blogsapi/BlogsApiApplication.java
+++ src/main/java/com/sanket/blogsapi/BlogsApiApplication.java
@@ -1,7 +1,11 @@
 package com.sanket.blogsapi;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -10,6 +14,12 @@ public class BlogsApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BlogsApiApplication.class, args);
+    }
+
+    @Bean
+    @Scope(BeanDefinition.SCOPE_SINGLETON)
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 
 }
