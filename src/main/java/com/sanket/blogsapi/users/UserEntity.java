bf23a7c99b312b00e16c2ff85a944015a936566b
sanketwakhare <sanketwakhare@gmail.com>
2023-05-14 22:08:49 +0530

    update mapping users_followings and add basic validations for fields

--- src/main/java/com/sanket/blogsapi/users/UserEntity.java
+++ src/main/java/com/sanket/blogsapi/users/UserEntity.java
@@ -1,14 +1,18 @@
 package com.sanket.blogsapi.users;
 
 import com.sanket.blogsapi.common.BaseEntity;
+import com.sanket.blogsapi.users.constants.UsersErrorMessages;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToMany;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.util.List;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @RequiredArgsConstructor
@@ -18,40 +22,46 @@ import java.util.List;
 public class UserEntity extends BaseEntity {
 
     @NonNull
+    @Size(min = 3, max = 30,
+            message = UsersErrorMessages.USERNAME_SIZE_CONSTRAINT_VIOLATION_ERROR)
     @Column(name = "username", nullable = false, unique = true, length = 30)
     private String username;
 
     @NonNull
+    @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}",
+            message = UsersErrorMessages.INVALID_USER_EMAIL)
     @Column(name = "email", nullable = false, unique = true, length = 30)
     private String email;
 
     @NonNull
-    @Setter
+    @Size(min = 6, max = 30,
+            message = UsersErrorMessages.PASSWORD_SIZE_CONSTRAINT_VIOLATION_ERROR)
     @Column(name = "password", nullable = false, length = 30)
     private String password;
 
-    @Setter
     @Column
     private String bio;
 
     @ManyToMany(targetEntity = UserEntity.class, mappedBy = "following")
-//    @JoinTable(
-//            name = "user_followers",
-//            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
-//            inverseJoinColumns = @JoinColumn(name = "follower_id", referencedColumnName = "id")
-//    )
-//    @JoinTable(
-//            name = "followers_followings",
-//            joinColumns = @JoinColumn(name = "following_id", referencedColumnName = "id"),
-//            inverseJoinColumns = @JoinColumn(name = "follower_id", referencedColumnName = "id")
-//    )
     private List<UserEntity> followers;
 
     @ManyToMany
-//    @JoinTable(
-//            name = "user_following",
-//            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
-//            inverseJoinColumns = @JoinColumn(name = "following_id", referencedColumnName = "id")
-//    )
     private List<UserEntity> following;
+
+//    if two separate mapping tables are needed for followers and following, then use the below code
+    /* @ManyToMany
+    @JoinTable(
+            name = "user_followers",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "follower_id", referencedColumnName = "id")
+    )
+    private List<UserEntity> followers;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_following",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "following_id", referencedColumnName = "id")
+    )
+    private List<UserEntity> following; */
 }
