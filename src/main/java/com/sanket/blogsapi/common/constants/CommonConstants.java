9211b0f0b42edc41826a6f7e6bc3d233d5abf8ba
sanketwakhare <sanketwakhare@gmail.com>
2023-05-15 22:36:15 +0530

    add common constants

---
+++ src/main/java/com/sanket/blogsapi/common/constants/CommonConstants.java
@@ -0,0 +1,34 @@
+package com.sanket.blogsapi.common.constants;
+
+public class CommonConstants {
+    public static final String EMPTY_STRING = "";
+    public static final String SPACE = " ";
+    public static final String HYPHEN = "-";
+    public static final String SLASH = "/";
+    public static final String UNDERSCORE = "_";
+    public static final String DOT = ".";
+    public static final String COLON = ":";
+    public static final String COMMA = ",";
+    public static final String SEMI_COLON = ";";
+    public static final String NEW_LINE = "\n";
+    public static final String EQUALS = "=";
+    public static final String AMPERSAND = "&";
+    public static final String QUESTION_MARK = "?";
+    public static final String AT_THE_RATE = "@";
+    public static final String HASH = "#";
+    public static final String DOLLAR = "$";
+    public static final String PERCENTAGE = "%";
+    public static final String CARET = "^";
+    public static final String AND = "and";
+    public static final String OR = "or";
+    public static final String NOT = "not";
+    public static final String TRUE = "true";
+    public static final String FALSE = "false";
+    public static final String YES = "yes";
+    public static final String NO = "no";
+    public static final String SUCCESS = "success";
+    public static final String FAILURE = "failure";
+    public static final String ERROR = "error";
+    public static final String WARNING = "warning";
+    public static final String INFO = "info";
+}
