4f443dd084ed4c96c33e3106041d69a59b7a5ea3
sanketwakhare <sanketwakhare@gmail.com>
2023-05-15 22:44:57 +0530

    rename file

--- src/main/java/com/sanket/blogsapi/services/tokens/exceptions/InvalidTokenException.java
+++ src/main/java/com/sanket/blogsapi/services/tokens/exceptions/InvalidTokenException.java
@@ -1,15 +1,15 @@
 package com.sanket.blogsapi.services.tokens.exceptions;
 
-import com.sanket.blogsapi.services.tokens.constants.TokensErrorConstants;
+import com.sanket.blogsapi.services.tokens.constants.TokensErrorMessages;
 
 public class InvalidTokenException extends RuntimeException {
 
     public InvalidTokenException() {
-        super(TokensErrorConstants.INVALID_AUTHORIZATION_TOKEN);
+        super(TokensErrorMessages.INVALID_AUTHORIZATION_TOKEN);
     }
 
     public InvalidTokenException(String token) {
-        super(String.format(TokensErrorConstants.INVALID_AUTHORIZATION_TOKEN_ID, token));
+        super(String.format(TokensErrorMessages.INVALID_AUTHORIZATION_TOKEN_ID, token));
     }
 
 }
