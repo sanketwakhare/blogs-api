4fe09aa432eaac73741f5f4b829db4dc588dfcba
sanketwakhare <sanketwakhare@gmail.com>
2023-05-16 15:20:29 +0530

    add generic sort criteria

---
+++ src/main/java/com/sanket/blogsapi/common/sort/SortCriteria.java
@@ -0,0 +1,20 @@
+package com.sanket.blogsapi.common.sort;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SortCriteria {
+
+    // TODO: add validation for field
+    // TODO: add different strategies for sorting based on field
+    // private String field;
+
+    @Enumerated(value = EnumType.STRING)
+    private SortDirection direction;
+}
---
+++ src/main/java/com/sanket/blogsapi/common/sort/SortDirection.java
@@ -0,0 +1,6 @@
+package com.sanket.blogsapi.common.sort;
+
+public enum SortDirection {
+    ASC,
+    DESC
+}
