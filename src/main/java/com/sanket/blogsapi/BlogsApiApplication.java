ff408eddc23f89fd5d494cc29e04ed1551e082fb
sanketwakhare <sanketwakhare@gmail.com>
2023-05-21 00:05:36 +0530

    configure token service bean

--- src/main/java/com/sanket/blogsapi/BlogsApiApplication.java
+++ src/main/java/com/sanket/blogsapi/BlogsApiApplication.java
@@ -1,6 +1,12 @@
 package com.sanket.blogsapi;
 
+import com.sanket.blogsapi.services.jwt.JwtTokenService;
+import com.sanket.blogsapi.services.tokens.TokenService;
+import com.sanket.blogsapi.services.tokens.sst.UserTokenRepository;
+import com.sanket.blogsapi.services.tokens.sst.UserTokenService;
+import com.sanket.blogsapi.users.UsersService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -28,6 +34,22 @@ public class BlogsApiApplication {
     @Scope(BeanDefinition.SCOPE_SINGLETON)
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // TODO: configure this in properties file
+    private final static String TOKEN_SERVICE_TYPE = "JWT"; // "SST" or "JWT"
+
+    @Bean
+    @Scope(BeanDefinition.SCOPE_SINGLETON)
+    public TokenService tokenService(@Autowired UsersService usersService,
+                                     @Autowired UserTokenRepository userTokenRepository) {
+        if (TOKEN_SERVICE_TYPE.equals("JWT")) {
+            return new JwtTokenService();
+        } else if (TOKEN_SERVICE_TYPE.equals("SST")) {
+            return new UserTokenService(usersService, userTokenRepository);
+        } else {
+            throw new RuntimeException("Invalid token service type");
+        }
     }
 
 }
