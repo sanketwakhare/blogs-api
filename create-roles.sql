3c8e01a5b31732d6260111a08895b7fd2935d10d
sanketwakhare <sanketwakhare@gmail.com>
2023-05-27 15:55:35 +0530

    update startup sql

--- create-roles.sql
+++ create-roles.sql
@@ -9,3 +9,22 @@ values (current_timestamp(),
         current_timestamp(),
         'USER',
         UUID_TO_BIN(uuid()));
+
+/*create super admin user*/
+insert into users (created_at, updated_at, username, email, password, id)
+values (current_timestamp(),
+        current_timestamp(),
+        'superadmin',
+        'superadmin@example.com',
+        ('$2a$10$mxjorpLyLHaLdqVQNphhNuLqXRDVR1TIVUKeYiIjVr92jvj3mI.cm'),
+        UUID_TO_BIN(uuid()));
+
+select id, username into @SuperAdminUserId, @SuperAdminUsername from users where username = 'superadmin';
+select @SuperAdminUserid, @SuperAdminUsername;
+
+select id, role into @AdminRoleId, @AdminRole from roles where role = 'ADMIN';
+select @AdminRoleId, @AdminRole;
+
+/*add ADMIN role for a superadmin user*/
+insert into users_roles (users_id, roles_id)
+values (@SuperAdminUserId, @AdminRoleId);
